e67116b34e4d8884c59a3147cc6968f197741cd3
David van Leeuwen <davz0rs@gmail.com>
2012-05-04 17:22:09 +0200

    Making stuff work

--- js/library.js
+++ js/library.js
@@ -1,10 +1,31 @@
+function startAlbum(e) {
+  var album = e.data.album;
+  var player = e.data.player;
+
+  //Default Track
+  var t = 0;
+
+  //If there's a starred track. Select that one, otherwise play first track
+  for (var i=0; i <= album.tracks.length; i++) {
+    if(album.tracks[i].starred == true){
+      t = i;
+      break;
+    }
+  }
+
+  player.track = album.tracks[t];
+
+  album.get = function() {
+    return album.tracks[t];
+  }
+}
+
 function getLibrary(){
-  //Sort albums by name
-  var albums = m.library.albums.sort(function(album1, album2) {
-    var name1 = album1.artist.name || "";
-    var name2 = album2.artist.name || "";
-    return name1.toLocaleLowerCase().localeCompare(name2.toLocaleLowerCase());
+  var albums = _.sortBy(m.library.albums, function(album) {
+    return album.data.artist.name.toLowerCase()
   });
+
+  var albums = albums.reverse();
 
   //Create a view for every album
   for (var i = albums.length - 1; i >= 0; i--) {
@@ -14,31 +35,14 @@ function getLibrary(){
 }
 
 function renderAlbumView(source){
-  
-
   var a = m.Album.fromURI(source, function(album) {
-    
-    //Default Track
-    var t = 0;
-
-    //If there's a starred track. Select that one, otherwise play first track
-    for (var i=0; i <= album.tracks.length; i++) {
-      if(album.tracks[i].starred == true){
-        t = i;
-        break;
-      }
-    }
-
     //Set track and album context
     var player = new v.Player();
-    player.track = album.tracks[t];
-    album.get = function() {
-        return album.tracks[t];
-    }
     player.context = album;
-
+    
     //Append node to document
     $(player.node).append("<a href='"+album.uri+"' class='albumName'>"+album.name+"</a><a href='"+album.artist.uri+"' class='artistName'>"+album.artist.name+"</div>");
+    $(player.node).find(".sp-player-button").on("click", {player: player, album: album}, startAlbum);
     $('#library').append(player.node);
     fillScreen();
   });
